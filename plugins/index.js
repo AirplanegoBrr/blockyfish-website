06afff3170742245714d5c557a98448ade382dd9
pi <90981829+ThePiGuy3141@users.noreply.github.com>
2022-10-27 16:03:29 -0700

    finished plugins page

--- plugins/index.js
+++ plugins/index.js
@@ -1,9 +1,41 @@
 function download(link) {
-    var element = document.createElement('a')
-    element.setAttribute('href', link)
-    element.style.display = 'none'
-    document.body.appendChild(element)
-    element.target = "_blank"
-    element.click()
-    document.body.removeChild(element)
+	var element = document.createElement("a");
+	element.setAttribute("href", link);
+	element.style.display = "none";
+	document.body.appendChild(element);
+	element.target = "_blank";
+	element.click();
+	document.body.removeChild(element);
 }
+
+var plugin_search = document.querySelector("form.search-box input");
+function filter_cards() {
+	var cards = document.getElementsByClassName("card");
+	if (plugin_search.value == "") {
+		for (let i = 0; i < cards.length; i++) {
+			cards[i].style.display = "block";
+		}
+	} else {
+		for (let i = 0; i < cards.length; i++) {
+			var plugin_name = document.querySelector(
+				".card:nth-child(" + (i + 1) + ") .card-info-title h3"
+			).innerText;
+			if (
+				!plugin_name.toLowerCase().includes(plugin_search.value.toLowerCase())
+			) {
+				cards[i].style.display = "none";
+			} else {
+				cards[i].style.display = "block";
+			}
+		}
+	}
+}
+plugin_search.addEventListener("keyup", () => {
+	filter_cards();
+});
+document
+	.querySelector("#title-banner form > button:nth-child(4)")
+	.addEventListener("click", () => {
+		plugin_search.value = "";
+		filter_cards();
+	});
