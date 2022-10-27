a1c42ef57cf63ec0b92b710f49f0f779f12dbb9a
pi <90981829+ThePiGuy3141@users.noreply.github.com>
2022-10-27 16:48:50 -0700

    fixed plugin download

--- plugins/index.js
+++ plugins/index.js
@@ -1,6 +1,7 @@
 function download(link) {
 	var element = document.createElement("a");
 	element.setAttribute("href", link);
+	element.setAttribute("download", "");
 	element.style.display = "none";
 	document.body.appendChild(element);
 	element.target = "_blank";
